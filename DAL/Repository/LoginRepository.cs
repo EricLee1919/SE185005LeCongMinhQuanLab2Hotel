842ae2630e864228c28d615d92c7343bd10fd93a
Lenovo <Lenovo@DESKTOP-N7QRRIC>
2025-07-19 08:48:50 +0700

    Add project files.

---
+++ DAL/Repository/LoginRepository.cs
@@ -0,0 +1,20 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository
+{
+    public class LoginRepository
+    {
+        private FuminiHotelManagementContext _context;
+
+        public LoginRepository()
+        {
+            _context = new();
+        }
+
+    }
+}
